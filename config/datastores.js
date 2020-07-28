6d01a5fc129adffa2b7b45b12465a7ac9e89d945
Mohammed Sahl <mskably@uwaterloo.ca>
2020-07-28 13:27:56 -0400

    Add mongodb datastore (#1)
    * Add mongodb datastore
    
    * Change id to mongoDB spec

--- config/datastores.js
+++ config/datastores.js
@@ -32,7 +32,7 @@ module.exports.datastores = {
   *                                                                          *
   ***************************************************************************/
 
-  default: {
+  mongodb: {
 
     /***************************************************************************
     *                                                                          *
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: require('sails-mongo'),
+    url: 'mongodb://localhost:27017/codeblobs',
 
   },
 
