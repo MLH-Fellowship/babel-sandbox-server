ab255d5e4d1df327cfffb86e59455040741fd7d8
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-07-29 11:36:22 -0700

    Make ID and Link funcs. Small refactor

--- snippet.js
+++ snippet.js
@@ -2,6 +2,29 @@ const crypto = require("crypto");
 
 const base64url = require("base64url");
 
+/**
+ * shortestURL returns a short URL based off a code.
+ * @param {string} code
+ * @returns {string}
+ */
+function shortestURL(code) {
+  // The resulting code is always at least 11 characters long.
+  // We just need to make sure that we don't end on a trailing _.
+  let hashLength = 11;
+  for (; hashLength <= code.length && code[hashLength - 1] == "_"; ) {
+    hashLength++;
+  }
+  return code.slice(0, hashLength);
+}
+
+/**
+ * newSeededHash returns a hash pre-seeded with the Snippets salt.
+ * @returns {crypto.Hash}
+ */
+function newSeededHash() {
+  return crypto.createHash("sha256").update(Snippet.salt);
+}
+
 class Snippet {
   static salt = "Babel Salt\n";
 
@@ -18,23 +41,28 @@ class Snippet {
   }
 
   /**
-   * ID returns a base64url encoded ID based off the current code snippet.
+   * Link returns the sharking link associated with the current snippet.
    * @returns {string} Base64URL Encoded String
    */
-  ID() {
-    const hash = crypto.createHash("sha256").update(Snippet.salt);
+  Link() {
+    const hash = newSeededHash();
     const values = [this.sourceCode, this.pluginSource, ...this.configs];
     values.forEach((str) => {
       hash.update(str);
     });
     const code = base64url(hash.digest());
-    // The resulting code is always at least 11 characters long.
-    // We just need to make sure that we don't end on a trailing _.
-    let hashLength = 11;
-    for (; hashLength <= code.length && code[hashLength - 1] == "_"; ) {
-      hashLength++;
-    }
-    return code.slice(0, hashLength);
+    return shortestURL(code);
+  }
+
+  /**
+   * ID generates a url friendly ID.
+   * @returns {string} Base64URL Encoded String
+   */
+  static ID(body) {
+    const hash = newSeededHash();
+    hash.update(body);
+    const code = base64url(hash.digest());
+    return shortestURL(code);
   }
 }
 
